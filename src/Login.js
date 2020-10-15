103f29226d07effb195339a9b6ec7da59a2930c6
jesusgomez545 <jesus.gomez@mercadolibre.cl>
2020-10-15 20:45:01 -0300

    Adding log for firebase app

--- src/Login.js
+++ src/Login.js
@@ -1,60 +1,71 @@
-import React, { useState, useContext } from "react";
-import { AuthContext } from "./index";
+import React, {useContext, useState} from "react";
+import {AuthContext} from "./index";
 import * as firebase from 'firebase'
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setErrors] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setErrors] = useState("");
 
-  const Auth = useContext(AuthContext);
-  const handleForm = e => {
+    const Auth = useContext(AuthContext);
+    const handleForm = e => {
 
-    e.preventDefault();
-    firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(res => {
-      console.log(res)
-      if (res.user) Auth.setLoggedIn(true);
-      
-    })
-    .catch(e => {
-      setErrors(e.message);
-    });
-  };
+        e.preventDefault();
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(res => {
+                console.log(res)
+                if (res.user) Auth.setLoggedIn(true);
 
-  return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={e => handleForm(e)}>
-        <input
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          name="email"
-          type="email"
-          placeholder="email"
-        />
-        <input
-          onChange={e => setPassword(e.target.value)}
-          name="password"
-          value={password}
-          type="password"
-          placeholder="password"
-        />
-        <hr />
-        <button className="googleBtn" type="button">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-            alt="logo"
-          />
-          Login With Google
-        </button>
-        <button type="submit">Login</button>
-        <span>{error}</span>
-      </form>
-    </div>
-  );
+            })
+            .catch(e => {
+                setErrors(e.message);
+            });
+
+        firebase
+            .auth()
+            .currentUser
+            .getIdToken(true)
+            .then(function (idToken) {
+                console.log(idToken)
+            })
+            .catch(function (error) {
+                console.log(error)
+            });
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={e => handleForm(e)}>
+                <input
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    name="email"
+                    type="email"
+                    placeholder="email"
+                />
+                <input
+                    onChange={e => setPassword(e.target.value)}
+                    name="password"
+                    value={password}
+                    type="password"
+                    placeholder="password"
+                />
+                <hr/>
+                <button className="googleBtn" type="button">
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                        alt="logo"
+                    />
+                    Login With Google
+                </button>
+                <button type="submit">Login</button>
+                <span>{error}</span>
+            </form>
+        </div>
+    );
 };
 
 export default Login;
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,1 +1,10 @@
-export default {}
+export default {
+    apiKey: "AIzaSyCV2QTqhETkoXgKJgPoxgZ0KjSMmuQ7EKM",
+    authDomain: "grille-backend.firebaseapp.com",
+    databaseURL: "https://grille-backend.firebaseio.com",
+    projectId: "grille-backend",
+    storageBucket: "grille-backend.appspot.com",
+    messagingSenderId: "247800737456",
+    appId: "1:247800737456:web:3fd2adba9062ccf65e1575",
+    measurementId: "G-QB8C1CZY1M"
+}
